53589677133e1e09e81d2a22b952223d86d40f8f
khaki-git <oscarpeachman+github@outlook.com>
2025-10-09 10:52:55 +1100

    First Commit

---
+++ Potions/Potions/src/Potions/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace Potions;
+
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log { get; private set; } = null!;
+
+    private void Awake()
+    {
+        Log = Logger;
+
+        Log.LogInfo($"Plugin {Name} is loaded!");
+    }
+}
